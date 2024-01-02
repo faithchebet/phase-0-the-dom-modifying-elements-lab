8631270a065a0dc4fd0a70837c837e1c9022651a
faithchebet <faith.chebet@student.moringaschool.com>
2024-01-02 13:41:28 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+// Write your code here
+document.addEventListener('DOMContentLoaded', function() {
+    const newHeader = document.createElement('h1');
+    newHeader.id = 'victory';
+    newHeader.innerHTML = 'YOUR-NAME is the champion';
+    document.body.appendChild(newHeader);
+  });
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    const newHeader = document.getElementById('victory');
+  });
+  
